a6f12c461f1851b7e042d13bf7fa65a6ee435255
Avinash Kulal <avinash.kulal@juegostudioz.com>
2022-06-10 14:55:36 +0530

    update login component

--- ReactApp/src/components/Login.js
+++ ReactApp/src/components/Login.js
@@ -1,8 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
+import styled from "styled-components";
 
-const Login = () =>{
+const LoginContainer = styled.div`
+background-color:#eee;
+height:300px;
+padding:10px;
+margin:5px;
+font-family:arial;
+`;
+const Login = (props) =>{
+
+ const [username,setUsername] = useState('');
+ const [password,setPassword] = useState('');
+
+ const handleLogin = (evt)=>{
+  evt.preventDefault();
+ }
  return (
-  <h3>Login page</h3>
+  <LoginContainer>
+   <h3>Login</h3><hr/><br/>
+   <form onSubmit={handleLogin}>
+     <input type="text" name="username" value={username} placeholder="Username.." size="40" onChange={(evt)=>{ setUsername(evt.target.value)}}/><br/><br/>
+     <input type="password" name="password" value ={password} placeholder="Password.." size="40"onChange={(evt)=>{setPassword(evt.target.value)}}/><br/><br/>
+     <input type="submit" value="Login"/>
+   </form>
+  </LoginContainer>
  )
 }
 export default Login;
