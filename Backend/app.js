cbdf873df89ec53634b5faac367921b5c1d90a94
Avinash Kulal <avinash.kulal@juegostudioz.com>
2022-06-10 17:11:59 +0530

    setup login endpoint

--- Backend/app.js
+++ Backend/app.js
@@ -3,12 +3,18 @@ const app = express();
 require("dotenv-defaults").config();
 const login = require("./routes/login");
 
+app.use(express.json());
+
 app.get('/',(req,res)=>{
  return res.send('Root endpoint');
 })
 
 app.use('/login',login);
 
+
+app.use("*",(req,res)=>{
+ res.status(500).json({  "message":"Invalid endpoint" })
+})
 
 app.listen(process.env.PORT,()=>{
  console.log(`App listen on port ${process.env.PORT}`)
--- Backend/routes/login.js
+++ Backend/routes/login.js
@@ -1,8 +1,23 @@
 const loginRoutes = require("express").Router();
-const users = require("../database/user")
+const userDb = require("../database/user")
 
-loginRoutes.post('/',(req,res)=>{
+loginRoutes.post('/', (req, res) => {
+ console.log('here')
+ const data = req.body;
+ if (!data.username || !data.password)
+  return res.status(500).json({
+   "message": "Username and password required"
+  })
+ var found = false;
+ userDb.users.map(user => {
+  if (user.username == data.username && user.password == data.password)
+   found = true;
+ })
 
+ if (found)
+  return res.status(200).json({"message": "user login successful"})
+ else
+  return res.status(400).json({"message": "user not found"})
 })
 
 module.exports = loginRoutes
