696b9705fd3ae4d2ef65d8ba66034059ccf147f9
Avinash Kulal <avinash.kulal@juegostudioz.com>
2022-06-10 12:36:22 +0530

    setup

---
+++ ReactApp/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import reactDom from "react-dom";
+import App from "./src/App";
+import { createRoot } from 'react-dom/client';
+
+const root = createRoot(document.getElementById('root'))
+root.render(<App/>)
---
+++ ReactApp/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const MinifyHtmlWebpackPlugin = require("minify-html-webpack-plugin")
+module.exports = {
+ mode: "development",
+ entry: "./index.js",
+ output: {
+  path: path.resolve(__dirname, "dist"),
+  filename: "index.js"
+ },
+ target: "web",
+ devServer: {
+  static: './dist',
+},
+ resolve: {
+  extensions: ['.js', '.jsx']
+ },
+ module: {
+  rules: [
+   {
+    test: /\.(js|jsx)/,
+    exclude: /node_modules/,
+    use: 'babel-loader'
+   },
+   {
+    test: /\.html/,
+    use: 'html-loader'
+   }
+
+  ]
+ },
+ //devtool: "eval-cheap-module-source-map",
+ devtool: 'inline-source-map',
+ plugins: [
+  new CopyPlugin({
+   patterns: [
+    { from: "./public/index.html", to: "." },
+   ],
+  }),
+  new MinifyHtmlWebpackPlugin({
+   src: './public',
+   dest: './dist',
+   rules: {
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    removeAttributeQuotes: true,
+    removeComments: true,
+    minifyJS: true,
+   }
+  })
+ ]
+}
