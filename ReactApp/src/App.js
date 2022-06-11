e802684e9bca4e7fc5bfb3ba37c797d295e80bc6
Avinash-Kulal <avinash.kulal@juegostudioz.com>
2022-06-11 15:58:55 +0530

    query parameter based component render

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React,{ useState,useEffect } from "react";
 import Login from "./components/Login";
 import Header from "./components/Header";
 
-const App = ()=>{
- return (
+const App = () => {
+  const [urlParam,setUrlParam] = useState('');
+ 
+  useEffect(()=>{
+    setUrlParamsState()
+  },[])
+
+  const setUrlParamsState = () =>{
+      const urlParams = new URLSearchParams(window.location.search);
+      setUrlParam({module:urlParams.get('module') })   
+  }
+
+  return (   
   <div>
     <Header/>
-    <Login/>
+    { (urlParam && urlParam.module === 'login')
+      ? <Login/> : <div>Hi</div>
+    }
+
+              
   </div>
    
- )
+  )
 }
 
 export default App;
